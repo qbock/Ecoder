9bec3bcab183a86f61721dd66941d5036c49e139
Quinlan Bock <quinlan.j.bock@gmail.com>
2023-04-11 22:00:10 -0500

    Add results files

--- plot.py
+++ plot.py
@@ -1,5 +1,6 @@
 from re import I
 import matplotlib.pyplot as plt
+from matplotlib.collections import PathCollection
 import csv
 import numpy as np
 import pandas as pd
@@ -19,6 +20,7 @@ parser.add_argument('-e',"--epoch", type=str, default='20', dest="epochs",
 
 baseline = {15: [6544, 1.3308, 0.393],
             20: [6544, 1.2385, 0.359],
+            30: [6544, 1.2385, 0.359],
             50: [6544, 1.1703, 0.339],
             100: [6544, 1.138, 0.324]
             }
@@ -46,31 +48,31 @@ class make_plots():
                 num_layers = int(i.split('[')[1].split(']')[0].count(','))+1
                 if num_layers == 0:
                         if labled[0]:
-                                self.sc.append(self.ax.scatter(x,y, color='lightsteelblue'))
+                                self.sc.append(self.ax.scatter(x,y, color='lightsteelblue', picker=True, pickradius=5))
                         else:
-                                self.sc.append(self.ax.scatter(x,y, color='lightsteelblue', label='0 CNN layer'))
+                                self.sc.append(self.ax.scatter(x,y, color='lightsteelblue', label='0 CNN layer', picker=True, pickradius=5))
                                 labled[0] = True
                 elif num_layers == 1:
                         if labled[1]:
-                                self.sc.append(self.ax.scatter(x,y, color='cornflowerblue'))
+                                self.sc.append(self.ax.scatter(x,y, color='cornflowerblue', picker=True, pickradius=5))
                         else:
-                                self.sc.append(self.ax.scatter(x,y, color='cornflowerblue', label='1 CNN layer'))
+                                self.sc.append(self.ax.scatter(x,y, color='cornflowerblue', label='1 CNN layer', picker=True, pickradius=5))
                                 labled[1] = True
                 elif num_layers == 2:
                         if labled[2]:
-                                self.sc.append(self.ax.scatter(x,y, color='royalblue'))
+                                self.sc.append(self.ax.scatter(x,y, color='royalblue', picker=True, pickradius=5))
                         else:
-                                self.sc.append(self.ax.scatter(x,y, color='royalblue', label='2 CNN layer'))
+                                self.sc.append(self.ax.scatter(x,y, color='royalblue', label='2 CNN layer', picker=True, pickradius=5))
                                 labled[2] = True
                 else:
                         if labled[3]:
-                                self.sc.append(self.ax.scatter(x,y, color='midnightblue'))
+                                self.sc.append(self.ax.scatter(x,y, color='midnightblue', picker=True, pickradius=5))
                         else:
-                                self.sc.append(self.ax.scatter(x,y, color='midnightblue', label='3 CNN layer'))
+                                self.sc.append(self.ax.scatter(x,y, color='midnightblue', label='3 CNN layer', picker=True, pickradius=5))
                                 labled[3] = True
 
         self.sc.append(self.ax.scatter(baseline_x,baseline_y, marker='*', c='gold', label="baseline model"))
-
+        print(self.sc)
         x_lower = min(X)*0.8
         # x_upper = max(X)/2
         x_upper = 5
@@ -97,6 +99,7 @@ class make_plots():
                 info = self.info.loc[idx]
                 print_model(emd,error,info)
 
+
 def print_model(emd, error, info):
     print("\n\n\t Metrics")
     print(f'EMD: {emd}\t EMD Error: {error}')
@@ -107,3 +110,39 @@ if __name__ == '__main__':
     args = parser.parse_args()
     make_plots(args)
     plt.show()
+
+        # For finding pareto front models
+        
+        # df = pd.read_csv('./results/run-6.csv')
+        # emd = df.loc[:,"EMD"]
+        # error =df.loc[:,"EMD_error"]
+        # ops =df.loc[:,"OPs"]
+        # info = df.loc[:,'info']
+        # bl = baseline[30]
+
+        # fig, ax = plt.subplots()
+        
+        # ax.set(xlim=(0.5, 3.0), ylim=(1e3, 30000))
+        
+        # lookup = list(zip(emd,ops,info))
+        # line, = ax.plot(emd,ops, 'o',
+        #                 picker=True, pickradius=5)
+        
+        # def onpick(event):
+        #         thisline = event.artist
+        #         xdata = thisline.get_xdata()
+        #         ydata = thisline.get_ydata()
+        #         ind = event.ind
+        #         x = xdata[ind]
+        #         y = ydata[ind]
+        #         print("**************************************************************************")
+        #         for item in lookup:
+        #                if (item[0] == x).any() and (item[1] == y).any():
+        #                       print(item)
+
+        
+        # fig.canvas.mpl_connect('pick_event', onpick)
+
+        # plt.show()
+
+
